e7ed91c958062d3ab839fd7f5794c1870aa0210d
Ana Ivan <ana.ivan.m@gmail.com>
2019-03-28 12:03:48 +0100

    Seeded and fixed paths

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,15 +1,10 @@
 class ProjectsController < ApplicationController
-  # PROJECTS = {
-  #   1 => { name: 'Flare', desc: 'Ruby on Rails, ActionCable, PostgreSQL, MapBox API', url: 'https://github.com/anaivanm/flare', image: 'project-flare.png' },
-  #   2 => { name: 'Portfolio website', desc: 'HTML, CSS, JS, GSAP, Bootstrap 4', url: 'https://github.com/anaivanm/flare', image: 'images/project-flare.png' },
-  #   3 => { name: 'Score Keeper', desc: 'Meteor, React, Semantic Ui', url: 'https://github.com/anaivanm/score-keeper-meteor', image: 'images/project-flare.png' }
-  # }
 
   def index
-    # @projects = PROJECTS
+    @projects = Project.all
   end
 
   def show
-  	# @project = PROJECTS[params[:id].to_i]
+  	@project = Project.find(params[:id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Project.create(name: 'Portfolio website', desc: 'HTML, CSS, JS, GSAP, Bootstrap 4', git_url: 'https://github.com/anaivanm/flare', web_url: 'www.sendaflare.com')
+Project.create(name: 'Score Keeper', desc: 'Meteor, React, Semantic Ui', git_url: 'https://github.com/anaivanm/score-keeper-meteor', web_url: 'www.sendaflare.com')
+Project.create(name: 'Flare', desc: 'Ruby on Rails, ActionCable, PostgreSQL, MapBox API', git_url: 'https://github.com/anaivanm/flare', web_url: 'www.sendaflare.com')
+
+
