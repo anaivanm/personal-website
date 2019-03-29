673baa92679bc96d61bcadf22a13f0bb53c8d341
Ana Ivan <ana.ivan.m@gmail.com>
2019-03-29 11:58:19 +0100

    WIP

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
 Project.destroy_all
 
-Project.create(name: 'Portfolio website', desc: 'HTML, CSS, JS, GSAP, Bootstrap 4', git_url: 'https://github.com/anaivanm/flare', web_url: 'www.sendaflare.com')
-Project.create(name: 'Score Keeper', desc: 'Meteor, React, Semantic Ui', git_url: 'https://github.com/anaivanm/score-keeper-meteor', web_url: 'www.sendaflare.com')
-Project.create(name: 'Flare', desc: 'Ruby on Rails, ActionCable, PostgreSQL, MapBox API', git_url: 'https://github.com/anaivanm/flare', web_url: 'www.sendaflare.com')
+puts "Adding projects..."
+
+projects = [
+	{ name: 'Portfolio website', desc: 'HTML, CSS, JS, GSAP, Bootstrap 4', git_url: 'https://github.com/anaivanm/flare', web_url: 'www.sendaflare.com', photo: 'project-flare.png' }, 
+	{ name: 'Score Keeper', desc: 'Meteor, React, Semantic Ui', git_url: 'https://github.com/anaivanm/score-keeper-meteor', web_url: 'www.sendaflare.com', photo: 'project-score.png' },
+	{ name: 'Flare', desc: 'Ruby on Rails, ActionCable, PostgreSQL, MapBox API', git_url: 'https://github.com/anaivanm/flare', web_url: 'www.sendaflare.com', photo: 'project-portfolio.jpg' },
+]	
 
 
+projects.each do |project|
+	Project.create(project)
+	print '*'
+end
+
+puts "Done!"
