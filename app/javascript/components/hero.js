27c87f69bf89554208ed2a55c71f1155c6a3c1de
Ana Ivan <ana.ivan.m@gmail.com>
2019-04-11 15:28:44 +0200

    Fix ui bugs homepage

--- app/javascript/components/hero.js
+++ app/javascript/components/hero.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
 	new Typed('#hero-typed-text', {
-		strings: ["Hi,^1200 I'm Ana. Nice to meet you! <br>^1000Have a look at some of my projects &mdash; "],
+		strings: ["Hi,^1200 I'm Ana. I'm a web developer. <br>^1000Have a look at some of my projects &mdash; "],
 		typeSpeed: 40,
 		loop: false,
 		showCursor: false
