99cd3ac4a4e3c0dfda6354e9fb1670b3ea55f740
Ana Ivan <ana.ivan.m@gmail.com>
2019-04-16 20:08:44 +0200

    Basic design all pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,12 @@ Project.destroy_all
 puts "Adding projects..."
 
 projects = [
-	{ name: 'Portfolio website', description: 'HTML, CSS, JS, GSAP, Bootstrap 4', git_url: 'https://github.com/anaivanm/flare', web_url: 'www.sendaflare.com', photo: 'project-flare.jpg' }, 
-	{ name: 'Score Keeper', description: 'Meteor, React, Semantic Ui', git_url: 'https://github.com/anaivanm/score-keeper-meteor', web_url: 'www.sendaflare.com', photo: 'project-score.jpg' },
-	{ name: 'Flare', description: 'Ruby on Rails, ActionCable, PostgreSQL, MapBox API', git_url: 'https://github.com/anaivanm/flare', web_url: 'www.sendaflare.com', photo: 'project-fcc-final.jpg' },
-	{ name: 'Cocktail Master', description: 'Ruby on Rails, PostgreSQL', git_url: 'https://github.com/anaivanm/flare', web_url: 'www.sendaflare.com', photo: 'project-fcc-final.jpg' },
-	{ name: 'Copenhagen Wine', description: 'Ruby on Rails, PostgreSQL', git_url: 'https://github.com/anaivanm/flare', web_url: 'www.sendaflare.com', photo: 'project-fcc-final.jpg' },
-	{ name: 'Airbnb Clone', description: 'Ruby on Rails, PostgreSQL', git_url: 'https://github.com/anaivanm/flare', web_url: 'www.sendaflare.com', photo: 'project-fcc-final.jpg' }
+	{ name: 'Portfolio website', description: 'HTML, CSS, JS, GSAP, Bootstrap 4', git_url: 'https://github.com/anaivanm/flare', web_url: 'www.sendaflare.com', photo: 'color-project-flare.png' }, 
+	{ name: 'Score Keeper', description: 'Meteor, React, Semantic Ui', git_url: 'https://github.com/anaivanm/score-keeper-meteor', web_url: 'www.sendaflare.com', photo: 'color-project-score.png' },
+	{ name: 'Flare', description: 'Ruby on Rails, ActionCable, PostgreSQL, MapBox API', git_url: 'https://github.com/anaivanm/flare', web_url: 'www.sendaflare.com', photo: 'color-project-portfolio.png' },
+	{ name: 'Cocktail Master', description: 'Ruby on Rails, PostgreSQL', git_url: 'https://github.com/anaivanm/flare', web_url: 'www.sendaflare.com', photo: 'color-project-flare.png' },
+	{ name: 'Copenhagen Wine', description: 'Ruby on Rails, PostgreSQL', git_url: 'https://github.com/anaivanm/flare', web_url: 'www.sendaflare.com', photo: 'color-project-flare.pngg' },
+	{ name: 'Airbnb Clone', description: 'Ruby on Rails, PostgreSQL', git_url: 'https://github.com/anaivanm/flare', web_url: 'www.sendaflare.com', photo: 'color-project-flare.png' }
 ]	
 
 
