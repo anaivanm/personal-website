616cc9e99aa3d6303c79f34fe782be5c0fbe4ce3
Ana Ivan <ana.ivan.m@gmail.com>
2019-04-20 20:16:27 +0200

    Partial redesign

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ projects = [
 	{ name: 'Score Keeper', description: 'Meteor, React, Semantic Ui', git_url: 'https://github.com/anaivanm/score-keeper-meteor', web_url: 'www.sendaflare.com', photo: 'color-project-score.png' },
 	{ name: 'Flare', description: 'Ruby on Rails, ActionCable, PostgreSQL, MapBox API', git_url: 'https://github.com/anaivanm/flare', web_url: 'www.sendaflare.com', photo: 'color-project-portfolio.png' },
 	{ name: 'Cocktail Master', description: 'Ruby on Rails, PostgreSQL', git_url: 'https://github.com/anaivanm/flare', web_url: 'www.sendaflare.com', photo: 'color-project-flare.png' },
-	{ name: 'Copenhagen Wine', description: 'Ruby on Rails, PostgreSQL', git_url: 'https://github.com/anaivanm/flare', web_url: 'www.sendaflare.com', photo: 'color-project-flare.pngg' },
+	{ name: 'Copenhagen Wine', description: 'Ruby on Rails, PostgreSQL', git_url: 'https://github.com/anaivanm/flare', web_url: 'www.sendaflare.com', photo: 'color-project-flare.png' },
 	{ name: 'Airbnb Clone', description: 'Ruby on Rails, PostgreSQL', git_url: 'https://github.com/anaivanm/flare', web_url: 'www.sendaflare.com', photo: 'color-project-flare.png' }
 ]	
 
